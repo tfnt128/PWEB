812fa4d4b04320995cda2679fbf0998b136f8482
Thiago Fontoura <87226253+tfnt128@users.noreply.github.com>
2025-11-17 12:44:27 -0300

    Delete Atividade14/PwebNode/Exercicios directory

--- Atividade14/PwebNode/Exercicios/exercicio3.js
+++
@@ -1,5 +0,0 @@
-let sum = 0;
-for(let i = 2; i <= process.argv.length - 1; i++){
-    sum += Number(process.argv[i]);
-}
-console.log("soma = " + sum);
--- Atividade14/PwebNode/Exercicios/exercicio4S.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const data = fs.readFileSync('cidades.txt');
-
-console.log(data.toString());
