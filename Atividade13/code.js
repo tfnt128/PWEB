3255debbcbffac6ab45a29d980894eaac7ac6859
Thiago Fontoura <87226253+tfnt128@users.noreply.github.com>
2025-11-17 09:42:45 -0300

    Add files via upload

---
+++ Atividade13/code.js
@@ -0,0 +1,14 @@
+function fechada(){
+    var img = document.getElementById("img");
+    img.src = "./imgs/janfechada.jpg"
+}
+
+function abrir(){
+    var img = document.getElementById("img");
+    img.src = "./imgs/janaberta.jpg"
+}
+
+function quebrar(){
+    var img = document.getElementById("img");
+    img.src = "./imgs/janquebrada.jpg"
+}
